c45aa27c083a71b3328bdb9a8d6ff0717b79a5d9
Albert Still <albertjamesstill@gmail.com>
2015-07-09 12:53:55 +0100

    Add root GET endpoint with app details

--- app/api.js
+++ app/api.js
@@ -9,6 +9,16 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+server.get('/', (req, res, next) => {
+  var app_details = {
+    author: 'https://github.com/albertstill',
+    repo: 'https://github.com/albertstill/omdb-graphql',
+    blog: 'http://red-badger.com/blog/2015/07/09/grahpql-and-the-open-movie-database-from-introspection-to-inception/'
+  }
+
+  res.send(app_details)
+});
+
 server.post('/', (req, res, next) => {
   graphql(MovieSchema, req.body).then((result) => {
     res.send(result)
